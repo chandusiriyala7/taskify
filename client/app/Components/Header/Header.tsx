f560bbd81014bfe9831c22f47ac97d6e6787df76
chandusiriyala <chandusiriyala116@gmail.com>
2024-12-03 14:10:54 +0530

    back chnages

--- client/app/Components/Header/Header.tsx
+++ client/app/Components/Header/Header.tsx
@@ -5,22 +5,16 @@ import { github, moon, profile } from "@/utils/Icons";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
-import Cookies from "js-cookie"; // For handling cookies
 
 function Header() {
-  const { user, logout } = useUserContext(); // Assuming logout function is provided in the user context
+  const { user } = useUserContext();
   const { openModalForAdd, activeTasks } = useTasks();
+
   const router = useRouter();
 
   const { name } = user;
-  const userId = user._id;
 
-  const handleLogout = () => {
-    // Clear user session (example: removing token from cookies or context)
-    Cookies.remove("authToken"); // Assuming the auth token is stored in a cookie
-    logout(); // This will clear the user context (add this function in userContext)
-    router.push("/login"); // Redirect to login page after logout
-  };
+  const userId = user._id;
 
   return (
     <header className="px-6 my-4 w-full flex items-center justify-between bg-[#f9f9f9]">
@@ -46,37 +40,20 @@ function Header() {
         </p>
       </div>
       <div className="h-[50px] flex items-center gap-[10.4rem]">
-        {userId ? (
-          <button
-            className="px-8 py-3 bg-[#3aafae] text-white rounded-[50px]
-            hover:bg-[#00A1F1] hover:text-white transition-all duration-200 ease-in-out"
-            onClick={() => {
+        <button
+          className="px-8 py-3 bg-[#3aafae] text-white rounded-[50px]
+          hover:bg-[#00A1F1] hover:text-white transition-all duration-200 ease-in-out"
+          onClick={() => {
+            if (userId) {
               openModalForAdd();
-            }}
-          >
-            Add a new Task
-          </button>
-        ) : (
-          <button
-            className="px-8 py-3 bg-[#3aafae] text-white rounded-[50px]
-            hover:bg-[#00A1F1] hover:text-white transition-all duration-200 ease-in-out"
-            onClick={() => {
+            } else {
               router.push("/login");
-            }}
-          >
-            Login / Register
-          </button>
-        )}
-
-        {userId && (
-          <button
-            className="px-8 py-3 bg-[#FF4D4D] text-white rounded-[50px]
-            hover:bg-[#FF0000] hover:text-white transition-all duration-200 ease-in-out"
-            onClick={handleLogout}
-          >
-            Logout
-          </button>
-        )}
+            }
+          }}
+        >
+          {userId ? "Add a new Task" : "Login / Register"}
+           
+        </button>
 
         <div className="flex gap-4 items-center">
           <Link
